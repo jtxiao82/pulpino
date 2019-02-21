9337d90af9d8af87f54a7fe417c81e463d1a147b
jtxiao82 <xiou8209@gmail.com>
2019-02-21 20:19:36 +0800

    Update the manual fault injection on PULPino top level by instrumentingw the source code

--- rtl/pulpino_top.sv
+++ rtl/pulpino_top.sv
@@ -112,6 +112,196 @@ module pulpino_top
   logic        rstn_int;
   logic [31:0] boot_addr_int;
 
+  /*******************************/
+  /*                             */
+  /* Fault Injection Block Begin */
+  /*                             */
+  /*******************************/
+  reg [31:0] ram [0:63];
+  integer f_regfile, f_regfile_dump;
+  integer i;
+  
+  /****************************/
+  /*                          */
+  /* Read fault configuration */
+  /*                          */
+  /****************************/
+  initial begin 
+    $readmemh("/home/m105/m105061614/NTHU/Research/Git/pulpino/sw/build/fault_config.data", ram);
+  end
+
+  /*******************/
+  /*                 */
+  /* Prevent hang on */
+  /*                 */
+  /*******************/
+  //parameter simtime = 1273800ns;   // gcd
+  //parameter simtime = 1973800ns;   // sw gcd + hw gcd
+  parameter simtime = 48102880ns;  // adaline
+  //parameter simtime   = 481028800ns; // qsort
+
+  parameter timeout   = simtime;
+
+  always @(posedge clk) begin
+    if($realtime > simtime + timeout && ram[0] == 1) begin
+      $stop;
+    end
+  end
+
+  /***************************/
+  /*                         */
+  /*   Dump register file    */
+  /*                         */
+  /***************************/
+  initial begin
+    f_regfile_dump = $fopen("trace_core_regfile_dump.log", "w");
+  end
+  final begin
+    $fclose(f_regfile_dump);
+  end
+  always @(negedge clk) begin 
+    if(ram[0] == 0) begin
+      $fwrite(f_regfile_dump, "%d,", core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles);
+      
+      if(ram[1] == core_region_i.CORE.RISCV_CORE.regfile_waddr_fw_wb_o && ram[1] != 0) begin
+        for(i = 0; i < ram[1] - 1; i = i + 1) begin
+          $fwrite(f_regfile_dump, "%8h,", core_region_i.CORE.RISCV_CORE.id_stage_i.registers_i.mem[i]);
+        end       
+        $fwrite(f_regfile_dump, "%8h,", core_region_i.CORE.RISCV_CORE.regfile_wdata);
+        for(i = ram[1] + 1; i < 32; i = i + 1) begin
+          $fwrite(f_regfile_dump, "%8h,", core_region_i.CORE.RISCV_CORE.id_stage_i.registers_i.mem[i]);
+        end
+      end else begin
+        for(i = 0; i < 32; i = i + 1) begin
+          $fwrite(f_regfile_dump, "%8h,", core_region_i.CORE.RISCV_CORE.id_stage_i.registers_i.mem[i]);
+        end
+      end
+      $fwrite(f_regfile_dump, "\n");
+    end   
+  end
+
+  /***************************/
+  /*                         */
+  /* Data Register Injection */
+  /*                         */
+  /***************************/
+  always @(negedge clk) begin
+    if(ram[0] == 1 && ram[5] == 0) begin // ram[0] => 0: fault-free sim, 1: fault sim, ram[5] => 0: datareg injection
+      if(core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles >= ram[3] && 
+         core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles < ram[4]) begin
+   
+        $display("PC: %25h, Datareg: %25d, Bitpos: %25d, BeginCycle: %25d, EndCycle: %25d", 
+                                           core_region_i.CORE.RISCV_CORE.pc_id+67200/*adaline offset*/, 
+                                           ram[1], ram[2], 
+                                           ram[3], ram[4]);
+   
+        if(ram[1] == core_region_i.CORE.RISCV_CORE.regfile_waddr_fw_wb_o) begin
+          $display("             %25d, we_en:%25d, re_req: %25d, %25d, %25h", core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles, 
+                                                                       core_region_i.CORE.RISCV_CORE.regfile_we_wb, 
+                                                                       core_region_i.CORE.RISCV_CORE.dbg_reg_rreq, 
+                                                                       core_region_i.CORE.RISCV_CORE.regfile_waddr_fw_wb_o, 
+                                                                       core_region_i.CORE.RISCV_CORE.regfile_wdata);
+          $fwrite(f_regfile_dump, "cycle,%25d,%d,%8h,\n", core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles, 
+                                                         core_region_i.CORE.RISCV_CORE.regfile_waddr_fw_wb_o, 
+                                                         core_region_i.CORE.RISCV_CORE.regfile_wdata);
+
+          force core_region_i.CORE.RISCV_CORE.regfile_wdata[ram[2]] = ~core_region_i.CORE.RISCV_CORE.regfile_wdata[ram[2]];
+
+          $display("             %25d, we_en:%25d, re_req: %25d, %25d, %25h", core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles, 
+                                                                 core_region_i.CORE.RISCV_CORE.regfile_we_wb, 
+                                                                 core_region_i.CORE.RISCV_CORE.dbg_reg_rreq, 
+                                                                 core_region_i.CORE.RISCV_CORE.regfile_waddr_fw_wb_o, 
+                                                                 core_region_i.CORE.RISCV_CORE.regfile_wdata);
+          $fwrite(f_regfile_dump, "cycle,%25d,%d,%8h,\n", core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles, 
+                                                  	 core_region_i.CORE.RISCV_CORE.regfile_waddr_fw_wb_o, 
+							 core_region_i.CORE.RISCV_CORE.regfile_wdata);
+        end else begin
+          $display("             %25d, re_req:%25d, %25d, %25h", core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles, 
+                                                    core_region_i.CORE.RISCV_CORE.dbg_reg_rreq,
+                                                    ram[1], 
+                                                    core_region_i.CORE.RISCV_CORE.id_stage_i.registers_i.mem[ram[1]]);       
+          $fwrite(f_regfile_dump, "cycle,%25d,%d,%8h,\n", core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles, 
+							 ram[1], 
+							 core_region_i.CORE.RISCV_CORE.id_stage_i.registers_i.mem[ram[1]]);
+
+          force core_region_i.CORE.RISCV_CORE.id_stage_i.registers_i.mem[ram[1]][ram[2]] = ~core_region_i.CORE.RISCV_CORE.id_stage_i.registers_i.mem[ram[1]][ram[2]];
+
+          $display("             %25d, re_req:%25d, %25d, %25h", core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles, 
+                                                                 core_region_i.CORE.RISCV_CORE.dbg_reg_rreq,
+                                                                 ram[1], 
+                                                                 core_region_i.CORE.RISCV_CORE.id_stage_i.registers_i.mem[ram[1]]);
+          $fwrite(f_regfile_dump, "cycle,%25d,%d,%8h,\n", core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles, 
+							 ram[1], 
+							 core_region_i.CORE.RISCV_CORE.id_stage_i.registers_i.mem[ram[1]]);
+        end
+    
+      end else begin
+        release core_region_i.CORE.RISCV_CORE.regfile_wdata;
+        release core_region_i.CORE.RISCV_CORE.id_stage_i.registers_i.mem;
+      end
+    end
+  end
+
+  /**************************************/
+  /*                                    */
+  /* CPU Pipeline IF/ID Latch Injection */
+  /*                                    */
+  /**************************************/
+  always @(negedge clk) begin
+    if(ram[0] == 1 && ram[5] == 2) begin // ram[0] => 0: fault-free sim, 1: fault sim, ram[5] => 2: IF Pipeline Latch Injection
+      if(core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles >= ram[3] && 
+         core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles < ram[4]) begin
+        $display("%d, IF Instr Rdata: %25h, Bitpos: %25d, BeginCycle: %25d, EndCycle: %25d", core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles, core_region_i.CORE.RISCV_CORE.if_stage_i.instr_rdata_id_o, ram[2], ram[3], ram[4]);
+
+        force core_region_i.CORE.RISCV_CORE.if_stage_i.instr_rdata_id_o[ram[2]] = ~core_region_i.CORE.RISCV_CORE.if_stage_i.instr_rdata_id_o[ram[2]];
+
+        $display("%d, IF Instr Rdata: %25h, Bitpos: %25d, BeginCycle: %25d, EndCycle: %25d", core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles, core_region_i.CORE.RISCV_CORE.if_stage_i.instr_rdata_id_o, ram[2], ram[3], ram[4]);
+      end else begin
+        release core_region_i.CORE.RISCV_CORE.if_stage_i.instr_rdata_id_o;
+      end
+    end
+  end
+
+  /*************************************************************/
+  /*                                                           */
+  /* Dump Regfile From Injection Cycle to Injection Cycle + 10 */
+  /*                                                           */
+  /*************************************************************/
+  initial begin
+    f_regfile = $fopen("trace_core_regfile_10_cycles.log", "w");
+  end
+  final begin
+    $fclose(f_regfile);
+  end
+
+  always @(negedge clk) begin
+    if(ram[0] == 1) begin
+      if(core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles >= ram[3] && 
+         core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles < ram[4]+10) begin
+        $fwrite(f_regfile, "cycles,%d,", core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles);
+        $fwrite(f_regfile, "%h,", core_region_i.CORE.RISCV_CORE.id_stage_i.pc_id_i+70884);
+        for(i = 0; i < 31; i = i + 1)
+          $fwrite(f_regfile, "%h,", core_region_i.CORE.RISCV_CORE.id_stage_i.registers_i.mem[i]);
+        $fwrite(f_regfile, "\n");
+        //if(core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles == ram[4]+9) // Early stop
+          //$stop();
+      end
+    end else if(ram[0] == 0) begin
+      $fwrite(f_regfile, "cycles,%d,", core_region_i.CORE.RISCV_CORE.riscv_tracer_i.cycles);
+      $fwrite(f_regfile, "%h,", core_region_i.CORE.RISCV_CORE.id_stage_i.pc_id_i+70884);
+      for(i = 0; i < 31; i = i + 1)
+        $fwrite(f_regfile, "%h,", core_region_i.CORE.RISCV_CORE.id_stage_i.registers_i.mem[i]);
+      $fwrite(f_regfile, "\n");
+    end
+  end
+  /*******************************/
+  /*                             */
+  /* Fault Injection Block End   */
+  /*                             */
+  /*******************************/
+
+
+
   AXI_BUS
   #(
     .AXI_ADDR_WIDTH ( `AXI_ADDR_WIDTH     ),
@@ -314,6 +504,8 @@ module pulpino_top
     .start_addr_i ( { 32'h1A10_0000, 32'h0010_0000, 32'h0000_0000 } ),
     .end_addr_i   ( { 32'h1A11_FFFF, 32'h001F_FFFF, 32'h000F_FFFF } )
   );
+
+
 
 endmodule
 
